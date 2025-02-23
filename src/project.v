141ec32fa95668ca74e8b4c1705aa057ab6b5125
jz6277 <jz6277@nyu.edu>
2025-02-23 00:28:35 -0500

    Update project.v

--- src/project.v
+++ src/project.v
@@ -17,8 +17,8 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-    assign uo_out = (ui_in[7] && uio_in[7]) ? ui_in + uio_in :
-                    (!ui_in[7] && !uio_in[7]) ? ui_in - uio_in :
+    assign uo_out = (ui_in[7] && uio_in[7]) ? ui_in[6:0] + uio_in[6:0] :
+        (!ui_in[7] && !uio_in[7]) ? ui_in[6:0] - uio_in[6:0] :
                                     0;
     assign uio_out = 0;
     assign uio_oe  = 0;
